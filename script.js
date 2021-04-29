28d1fae73db99536c2feadde20d2e4336c47f174
Nash Villarta <>
2021-04-29 15:10:06 +0000

    spaced end and between = of lightblue

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //  This is where the click of the button is registered
 document.getElementById('button').addEventListener('click', register)
 //  This edits the background color
-document.body.style.backgroundColor='lightblue'
+document.body.style.backgroundColor = 'lightblue'
 //  Here is where the click receives it's function. A alert shows up displaying the text and saying thanks for clicking.
 function register () {
   alert(document.getElementById('inputbox').value)
